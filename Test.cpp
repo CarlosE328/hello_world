0f2734a4f26dc9fed4d219942a9c2f429b61eb6a
CarlosE328 <carlos.espinosa328@hotmail.com>
2021-08-24 18:27:10 -0700

    first code

---
+++ Test.cpp
@@ -0,0 +1,27 @@
+
+// Find size of fundamental data types
+ 
+// Header file for input output functions
+#include<iostream>
+ 
+using namespace std;
+ 
+// main function -
+
+int main (void)
+{
+cout<< "Find Size of fundamental data types:\n------------------------------------\n";
+
+   cout << " The sizeof(char) is :       " << sizeof(char) << " bytes \n" ;
+   cout << " The sizeof(short) is :      " << sizeof(short) << " bytes \n" ;
+   cout << " The sizeof(int) is :        " << sizeof(int) << " bytes \n" ;
+   cout << " The sizeof(long) is :       " << sizeof(long) << " bytes \n" ;
+   cout << " The sizeof(long long) is :  " << sizeof(long long) << " bytes \n";
+   cout << " The sizeof(float) is :      " << sizeof(float) << " bytes \n" ;
+   cout << " The sizeof(double) is :     " << sizeof(double) << " bytes \n";
+   cout << " The sizeof(long double) is :" << sizeof(long double) << " bytes \n";
+   cout << " The sizeof(bool) is :       " << sizeof(bool) << " bytes \n\n";
+
+     
+
+}
